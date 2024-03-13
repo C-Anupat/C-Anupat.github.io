c887a4acbaa4980e4b056f3acac861b86bccd757
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-13 19:47:58 +0900

    Roll back index.html and try to implement client.js

--- js/client.js
+++ js/client.js
@@ -1,8 +1,11 @@
 TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
       return [{
-        icon: 'icon.png',
-        text: 'Test_PowerUp',
+        icon: 'https://raw.githubusercontent.com/C-Anupat/C-Anupat.github.io/master/icon.png',
+        text: "Hello",
+        callback: function(t){
+            return t.card().then(card => alert("In Progress..."))
+        }
       }];
     },
   });
