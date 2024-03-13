ce09a2c29ac97d4151377fdc552fd834976b24c5
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-13 19:59:17 +0900

    Try to change icon

--- js/client.js
+++ js/client.js
@@ -1,7 +1,7 @@
 TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
       return [{
-        icon: 'https://raw.githubusercontent.com/C-Anupat/C-Anupat.github.io/master/icon.png',
+        icon: 'https://raw.githubusercontent.com/C-Anupat/C-Anupat.github.io/0b5781c7baebf0061937dc4a3e49ab74a38cf448/icon.svg',
         text: "Hello",
         callback: function(t){
             return t.card().then(card => alert("In Progress..."))
