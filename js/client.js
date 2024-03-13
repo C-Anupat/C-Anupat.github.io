6826e5f75a3c484670eabe57f87f0bce30906c3e
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-13 19:33:42 +0900

    Try changing the icon path

--- js/client.js
+++ js/client.js
@@ -1,8 +1,8 @@
 TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
       return [{
-        icon: 'https://raw.githubusercontent.com/C-Anupat/C-Anupat.github.io/master/icon.png',
-        text: 'Tese_PowerUp',
+        icon: 'icon.png',
+        text: 'Test_PowerUp',
       }];
     },
   });
