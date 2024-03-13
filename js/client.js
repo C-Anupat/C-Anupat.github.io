1f4b2eb8898f99b2fc84aac7b8eefcb8ab14a994
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-13 19:17:10 +0900

    Add power-up button iFrame and implement

---
+++ js/client.js
@@ -0,0 +1,8 @@
+TrelloPowerUp.initialize({
+    'card-buttons': function(t, options){
+      return [{
+        icon: 'https://raw.githubusercontent.com/C-Anupat/C-Anupat.github.io/master/icon.png',
+        text: 'Tese_PowerUp',
+      }];
+    },
+  });
