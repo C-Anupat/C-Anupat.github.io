44d9bfb8b8b9f34325a9127e7fe746400c905a7c
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-13 20:10:52 +0900

    Implement resized icon.svg

--- js/client.js
+++ js/client.js
@@ -1,7 +1,7 @@
 TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
       return [{
-        icon: 'https://raw.githubusercontent.com/C-Anupat/C-Anupat.github.io/0b5781c7baebf0061937dc4a3e49ab74a38cf448/icon.svg',
+        icon: 'https://raw.githubusercontent.com/C-Anupat/C-Anupat.github.io/78803d29a5a5987bf0a513ede7107c7922469a88/icon.svg',
         text: "Hello",
         callback: function(t){
             return t.card().then(card => alert("In Progress..."))
