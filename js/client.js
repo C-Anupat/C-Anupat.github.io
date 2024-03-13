d370971e20d6a0fb81cc3ec6f763beb3b9e7b1af
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-13 20:23:47 +0900

    Try to use local icon.svg

--- js/client.js
+++ js/client.js
@@ -2,8 +2,8 @@ TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
       return [{
         // icon: 'https://raw.githubusercontent.com/C-Anupat/C-Anupat.github.io/78803d29a5a5987bf0a513ede7107c7922469a88/icon.svg',
-        icon : 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-        text: "Hello",
+        icon: 'icon.svg',
+        text: 'Hello',
         callback: function(t){
             return t.card().then(card => alert("In Progress..."))
         }
