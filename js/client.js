e5c59b940e620d5a512b5add155caabf24137ef6
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-14 10:07:05 +0900

    Implement DropBox

--- js/client.js
+++ js/client.js
@@ -2,10 +2,13 @@ TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
       return [{
         // icon: 'https://raw.githubusercontent.com/C-Anupat/C-Anupat.github.io/78803d29a5a5987bf0a513ede7107c7922469a88/icon.svg',
-        icon: 'icon.svg',
+        icon: './icon.svg',
         text: 'Hello',
         callback: function(t){
-            return t.card().then(card => alert("In Progress..."))
+            return t.popup()({
+                title: "DropBox",
+                url: "dropbox.html"
+            })
         }
       }];
     },
