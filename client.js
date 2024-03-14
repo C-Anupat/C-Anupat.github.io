bc4345ba8bc0b7ce2689fc8d99a981b52d21ee46
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-14 15:14:32 +0900

    Try to using on-enable to get data

--- client.js
+++ client.js
@@ -1,9 +1,14 @@
 TrelloPowerUp.initialize({
+    'on-enable': function(t, options){
+      var consoleWindowFrame = window.TrelloPowerUp.iframe();
+      return consoleWindowFrame.get('board', 'private').then(function (data){
+          console.log(JSON.stringify(data, null, 2));
+      });
+    },
     'card-buttons': function(t, options){
       return [{
         icon: 'https://c-anupat.github.io/icon.svg',
-        // icon: 'icon.svg',
-        text: 'Hello',
+        text: 'PowerUpTest',
         callback: function(t){
             return t.popup({
                 title: "DropBox",
