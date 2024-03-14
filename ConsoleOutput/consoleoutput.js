0f227706ae69940bfbf546eef5b8b299e78c108b
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-14 16:17:09 +0900

    Test t.get()

--- ConsoleOutput/consoleoutput.js
+++ ConsoleOutput/consoleoutput.js
@@ -1,6 +1,9 @@
 var t = window.TrelloPowerUp.iframe();
 
-return t.get('board', 'shared')
-.then(function (data) {
-  console.log(JSON.stringify(data, null, 2));
+t.render(function(){
+    t.get('board', 'shared')
+    .then(function (data) {
+      console.log(JSON.stringify(data, null, 2));
+    })
+    .then(t.size)
 });
--- client.js
+++ client.js
@@ -1,14 +1,16 @@
 TrelloPowerUp.initialize({
     'board-buttons': function(t, options){
-      var data = t.get('board', 'shared')
-      console.log(JSON.stringify(data, null, 2));
+      t.get('board', 'shared')
+      .then(function(data){
+        console.log(JSON.stringify(data, null, 2));
+      });
       return[{
         icon: 'https://c-anupat.github.io/icon.svg',
         text: 'PowerUpTest',
         callback: function(t){
             return t.modal({
                 title: "ConsoleOutput",
-                url: "./ConsoleOutput/consoleoutput.html"
+                // url: "./ConsoleOutput/consoleoutput.html"
             })
         }
       }]
