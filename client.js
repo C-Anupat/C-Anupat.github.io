0cb39419ffac402a273af9ddb4a02da01c52b63c
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-14 18:11:04 +0900

    Try to get API key

--- client.js
+++ client.js
@@ -1,16 +1,23 @@
+const listName = ["バックログ", "タスク", "進行中", "保留", "確認待ち", "完了"];
 var boardInfo;
-TrelloPowerUp.initialize({
+TrelloPowerUp.initialize(
+  {
+    appKey: "my-trello-key",
+    appName: "My Power-Up",
+    appAuthor: "My Company",
+  },
+  {
     'board-buttons': function(t, options){
       return[{
         icon: 'https://c-anupat.github.io/icon.svg',
         text: 'PowerUpTest',
         callback: function(t){
-          return t.board("all")
-          .then(function(board){
-            let boardInfo = board;
-            console.log("Board Name:" + boardInfo["name"]);
-            console.log("Board ID: " + boardInfo["id"]);
-          });
+          boardInfo = t.board("all");
+          console.log("Trello Key: " + appKey);
+          console.log("Power-Up Name: " + appName);
+          console.log("Company: " + appAuthor);
+          console.log("Board Name: " + boardInfo["name"]);
+          console.log("Board ID: " + boardInfo["id"]);
         }
       }];
     },
