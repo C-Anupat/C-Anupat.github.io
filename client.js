65dfd4c317ebd40ad720295b5eac00a32d476cde
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-14 13:41:25 +0900

    Implement new path

--- client.js
+++ client.js
@@ -2,12 +2,12 @@ TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
       return [{
         // icon: 'https://raw.githubusercontent.com/C-Anupat/C-Anupat.github.io/78803d29a5a5987bf0a513ede7107c7922469a88/icon.svg',
-        icon: './icon.svg',
+        icon: 'icon.svg',
         text: 'Hello',
         callback: function(t){
             return t.popup()({
                 title: "DropBox",
-                url: "dropbox.html"
+                url: "/DropBox/dropbox.html"
             })
         }
       }];
