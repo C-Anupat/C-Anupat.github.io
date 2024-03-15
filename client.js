97924047be07911bfd2ff1638004a585aa537700
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-15 14:32:49 +0900

    Try to using .env

--- client.js
+++ client.js
@@ -1,5 +1,9 @@
+const apiKey = process.env.API_KEY;
+const apiToken = process.env.API_TOKEN;
 const listName = ["バックログ", "タスク", "進行中", "保留", "確認待ち", "完了"];
+
 var boardInfo;
+
 TrelloPowerUp.initialize(
   {
     'board-buttons': function(t, options){
@@ -9,6 +13,8 @@ TrelloPowerUp.initialize(
         callback: function(t){
           return t.board("all").then(function(board){
             let boardInfo = board;
+            console.log("API Key: " + apiKey);
+            console.log("API Token: " + apiToken);
             console.log("Board Name: " + boardInfo["name"]);
             console.log("Board ID: " + boardInfo["id"]);
           });
