bbad0a4d918145988841b45c1cae0f3143b464f4
CHUNHAWIRIYAKUN ANUPAT <a-chunhawiriyakun@tripleone.net>
2024-03-15 12:11:47 +0900

    Try to get API key and Token

--- client.js
+++ client.js
@@ -1,15 +1,39 @@
 const listName = ["バックログ", "タスク", "進行中", "保留", "確認待ち", "完了"];
 var boardInfo;
+var apiKey;
+var apiToken;
 TrelloPowerUp.initialize(
+  {
+    appKey: "my-api-key",
+    appName: "My Trello App",
+    appAuthor: "My Company",
+  },
   {
     'board-buttons': function(t, options){
       return[{
         icon: 'https://c-anupat.github.io/icon.svg',
         text: 'PowerUpTest',
         callback: function(t){
-            let boardInfo = t.board("all");
-            console.log("Board Name: " + boardInfo["name"]);
-            console.log("Board ID: " + boardInfo["id"]);
+          return(t.getRestApi().getToken()
+          .then(function(token){
+            if(!token){
+              console.log("Fail to get token");
+            }
+            else{
+              let apiKey = appKey;
+              let apiToken = token;
+              console.log("API Key: " + apiKey);
+              console.log("API Token: " + apiToken);
+            }
+          }))
+          // boardPromise.then(function(board){
+          //   let boardInfo = board;
+          //   console.log("Board Name: " + boardInfo["name"]);
+          //   console.log("Board ID: " + boardInfo["id"]);
+          // })
+          // .then(function(board){
+            
+          // });
         }
       }];
     },
@@ -25,4 +49,5 @@ TrelloPowerUp.initialize(
         }
       }];
     },
-  });
+  }
+);
